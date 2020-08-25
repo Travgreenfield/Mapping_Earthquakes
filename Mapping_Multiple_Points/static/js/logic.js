95dfce9154adca60d7a90304b2710722b00199d5
Travgreenfield <travgreenfield@gmail.com>
2020-08-24 19:05:29 -0500

    Multiple map files

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -2,23 +2,24 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([34.0522, -118.2437], 14);
+let map = L.map('mapid').setView([34.0522, -118.2437], 4);
 
-// Create the map object with a center and zoom level.
-// let map = L.map("mapid", {
-//     center: [
-//       40.7, -94.5
-//     ],
-//     zoom: 4
-//   });
+// Get data from cities.js
+let cityData = cities;
 
-//  Add a marker to the map for Los Angeles, California.
-L.circleMarker([34.0522, -118.2437], {
-	color: 'black',
-	fillColor: '#F6F7B5',
-	fillOpacity: 0.5,
-	radius: 300
- }).addTo(map);
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+	console.log(city)
+	L.circleMarker(city.location, {
+		radius: city.population/200000,
+		color: 'orange',
+		lineweight: 4,
+		fillColor: '#FFD37C',
+		fillOpacity: 0.5,
+	})
+	.bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+	.addTo(map);
+   });
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
